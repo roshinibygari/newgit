a28e30b78dc0e291038d39ed9df7bf19da6ebda7
roshinibygari <roshinibygari@gmail.com>
2022-05-04 14:23:01 -0500

    new methods added to hellocontroller

---
+++ spring-boot-hello-world/src/main/java/com/mkyong/ByeController.java
@@ -0,0 +1,14 @@
+package com.mkyong;
+
+public class ByeController {
+	
+	
+	private String byee() {
+		return "telling bye";
+		
+	}
+	private int byee2() {
+		return 2;
+	}
+ 
+}
--- spring-boot-hello-world/src/main/java/com/mkyong/HelloController.java
+++ spring-boot-hello-world/src/main/java/com/mkyong/HelloController.java
@@ -4,11 +4,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//created a class hellocontroller
 public class HelloController {
 
-    @RequestMapping("/")
-    String hello() {
-        return "Hello World, Spring Boot!";
+	//created a method of string hello returns john
+   public String hello() {
+        return "John";
     }
-
+    public static void main(String[] args) {
+    	//new object is created
+    	HelloController h1=new HelloController();
+    	//assigning a variable "name"to the method "hello"
+    	String name = h1.hello();
+    	//printing the string 
+    	System.out.println(name);
+    	
+}
 }
